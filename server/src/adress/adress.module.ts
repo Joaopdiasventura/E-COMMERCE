0297cfc3a4eb79489afd2a49574e46b995cc50aa
João Paulo Dias <121265378+Joaopdiasventura@users.noreply.github.com>
2024-03-07 12:33:25 -0300

    fix: retirando as rotas de endereço

--- server/src/adress/adress.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AdressService } from './adress.service';
-import { AdressController } from './adress.controller';
-
-@Module({
-  controllers: [AdressController],
-  providers: [AdressService],
-})
-export class AdressModule {}
--- server/src/adress/entities/adress.entity.ts
+++
@@ -1,7 +0,0 @@
-export class Adress {
-    Endereco: string;
-    Bairro: string;
-    Cidade: string;
-    Estado: string;
-    Sigla: string;
-}
