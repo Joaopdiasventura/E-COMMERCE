6001a757c757b74b2a390976eef219f7499a6ae3
João Paulo Dias Ventura <joaopdiasventura@gmail.com>
2024-03-13 17:47:12 -0300

    feat: criando um container para a aplicação

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,17 +4,13 @@ import {
 	NestFastifyApplication,
 } from "@nestjs/platform-fastify";
 import { AppModule } from "./app.module";
-// import { UserModule } from "./modules/user.module";
-// import { ProductModule } from "./modules/product.module";
-// import { PurchaseModule } from "./modules/purchase.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create<NestFastifyApplication>(
 		AppModule,
 		new FastifyAdapter(),
 	);
-	await app.listen(3000);
+	await app.listen(10000);
 }
 
 bootstrap();
-
